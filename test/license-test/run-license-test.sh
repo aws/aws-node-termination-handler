178147ba4f3ee110a01300dabb1c8220121abff0
Brandon Wagner <wagnerbm@amazon.com>
2020-01-27 14:26:09 -0600

    fix supporting build scripts wrt make args

--- test/license-test/run-license-test.sh
+++ test/license-test/run-license-test.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 set -euo pipefail
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-BINARY_NAME="node-termination-handler"
-BINARY_CONTAINER="extract"
-BINARY_IMAGE_TAG="nth"
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+BUILD_BIN="$SCRIPTPATH/../../build/bin"
+
+BINARY_NAME="node-termination-handler-linux-amd64"
 LICENSE_TEST_TAG="nth-license-test"
 
-function clean_up {
-    docker container rm $BINARY_CONTAINER || :
-    docker image rm $BINARY_IMAGE_TAG || :
-    rm -f $SCRIPTPATH/$BINARY_NAME || :
-}
-
-trap "clean_up" EXIT
-
-docker build --build-arg=GOPROXY=direct -t $BINARY_IMAGE_TAG $SCRIPTPATH/../../
-docker container create --name $BINARY_CONTAINER $BINARY_IMAGE_TAG
-docker container cp $BINARY_CONTAINER:/$BINARY_NAME $SCRIPTPATH/$BINARY_NAME 
+NTH_OS_ARCH="linux-amd64" make -s -f $SCRIPTPATH/../../Makefile build-binaries
 docker build --build-arg=GOPROXY=direct -t $LICENSE_TEST_TAG $SCRIPTPATH/
-docker run -it -e GITHUB_TOKEN --rm -v $SCRIPTPATH/:/test $LICENSE_TEST_TAG golicense /test/license-config.hcl /test/$BINARY_NAME
+docker run -it -e GITHUB_TOKEN --rm -v $SCRIPTPATH/:/test -v $BUILD_BIN/:/nth-bin $LICENSE_TEST_TAG golicense /test/license-config.hcl /nth-bin/$BINARY_NAME
