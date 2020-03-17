e9786272351560736da5fd0db01fc237c3afed59
Matt Becker <mrbecker@amazon.com>
2020-03-17 09:34:30 -0500

    updating text proxy timeplus2min to be static
    updating text proxy timeplus2min to be static

--- test/ec2-metadata-test-proxy/cmd/ec2-metadata-test-proxy.go
+++ test/ec2-metadata-test-proxy/cmd/ec2-metadata-test-proxy.go
@@ -51,6 +51,7 @@ const (
 )
 
 var startTime int64 = time.Now().Unix()
+var spotInterruptionTime string = time.Now().Local().Add(time.Minute * time.Duration(2)).Format(time.RFC3339)
 
 // ScheduledEventDetail metadata structure for json parsing
 type ScheduledEventDetail struct {
@@ -143,9 +144,8 @@ func handleRequest(res http.ResponseWriter, req *http.Request) {
 			http.Error(res, "ec2-metadata-test-proxy feature not enabled", http.StatusNotFound)
 			return
 		}
-		timePlus2Min := time.Now().Local().Add(time.Minute * time.Duration(2)).Format(time.RFC3339)
 		instanceAction := InstanceAction{
-			Time:   timePlus2Min,
+			Time:   spotInterruptionTime,
 			Action: "terminate",
 		}
 		js, err := json.Marshal(instanceAction)
