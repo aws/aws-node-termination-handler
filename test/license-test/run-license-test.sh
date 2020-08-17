d4d584221ee3b69dc8c0bbe578e3071715fe6625
Brandon Wagner <wagnerbm@amazon.com>
2020-08-17 17:21:26 -0500

    only build one binary to check licenses

--- test/license-test/run-license-test.sh
+++ test/license-test/run-license-test.sh
@@ -7,6 +7,6 @@ BUILD_BIN="$SCRIPTPATH/../../build/bin"
 BINARY_NAME="node-termination-handler-linux-amd64"
 LICENSE_TEST_TAG="nth-license-test"
 
-NTH_OS_ARCH="linux-amd64" make -s -f $SCRIPTPATH/../../Makefile build-binaries
+SUPPORTED_PLATFORMS_LINUX="linux/amd64" make -s -f $SCRIPTPATH/../../Makefile build-binaries
 docker build --build-arg=GOPROXY=direct -t $LICENSE_TEST_TAG $SCRIPTPATH/
 docker run -it -e GITHUB_TOKEN --rm -v $SCRIPTPATH/:/test -v $BUILD_BIN/:/nth-bin $LICENSE_TEST_TAG golicense /test/license-config.hcl /nth-bin/$BINARY_NAME
