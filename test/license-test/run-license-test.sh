66464a525955ff3b3b8c6c959f2d87908f9429ec
Brandon Wagner <wagnerbm@amazon.com>
2021-06-14 09:26:00 -0500

    send license report to file first

--- test/license-test/run-license-test.sh
+++ test/license-test/run-license-test.sh
@@ -2,11 +2,14 @@
 set -euo pipefail
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-BUILD_BIN="$SCRIPTPATH/../../build/bin"
+BUILD_PATH="$SCRIPTPATH/../../build"
+BUILD_BIN="$BUILD_PATH/bin"
 
 BINARY_NAME="node-termination-handler-linux-amd64"
 LICENSE_TEST_TAG="nth-license-test"
+LICENSE_REPORT_FILE="$BUILD_PATH/license-report"
 
 SUPPORTED_PLATFORMS_LINUX="linux/amd64" make -s -f $SCRIPTPATH/../../Makefile build-binaries
 docker build --build-arg=GOPROXY=direct -t $LICENSE_TEST_TAG $SCRIPTPATH/
-docker run -i -e GITHUB_TOKEN --rm -v $SCRIPTPATH/:/test -v $BUILD_BIN/:/nth-bin $LICENSE_TEST_TAG golicense /test/license-config.hcl /nth-bin/$BINARY_NAME | tee /dev/tty | $SCRIPTPATH/check-licenses.sh
+docker run -i -e GITHUB_TOKEN --rm -v $SCRIPTPATH/:/test -v $BUILD_BIN/:/nth-bin $LICENSE_TEST_TAG golicense /test/license-config.hcl /nth-bin/$BINARY_NAME | tee $LICENSE_REPORT_FILE
+$SCRIPTPATH/check-licenses.sh $LICENSE_REPORT_FILE
