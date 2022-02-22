fde73f4f42bddafef5121d1358c1b3e73b467a15
Jerad C <jeradc@amazon.com>
2022-02-22 13:46:26 -0600

    add kubebuilder setup with setup-envtest

--- src/scripts/toolchain.sh
+++ src/scripts/toolchain.sh
@@ -14,6 +14,9 @@ usage: $(basename "$0") -h | -d PATH
 EOM
 )
 
+k8s_version="${K8S_VERSION:-1.21.x}"
+kubebuilder_assets_dir="${KUBEBUILDER_ASSETS:-$HOME/.kubebuilder/bin}"
+
 tools=(
     "github.com/google/ko@v0.9.3"
     "sigs.k8s.io/controller-tools/cmd/controller-gen@v0.8.0"
@@ -50,3 +53,13 @@ for tool in ${tools[@]}; do
     echo "Downloading $tool"
     GOBIN="$bin_dir" go install "$tool"
 done
+
+# Kubebuilder does not support darwin/arm64 so use amd64 through Rosetta instead.
+arch="$(go env GOARCH)"
+if [[ "$(go env GOOS)/$arch" == "darwin/arm64" ]]; then
+    arch="amd64"
+fi
+
+mkdir -p "$kubebuilder_assets_dir"
+ln -sf "$("$bin_dir/setup-envtest" use -p path "$k8s_version" --arch="$arch" --bin-dir="$kubebuilder_assets_dir")"/* "$kubebuilder_assets_dir"
+find "$kubebuilder_assets_dir"
