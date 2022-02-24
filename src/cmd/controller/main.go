31f8428a4f16bd79728fd4d18d1617f1e14ea5e1
Jerad C <jeradc@amazon.com>
2022-02-24 11:43:05 -0600

    use knative in controller

--- src/cmd/controller/main.go
+++ src/cmd/controller/main.go
@@ -18,7 +18,6 @@ package main
 
 import (
 	"flag"
-	"os"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
@@ -26,20 +25,27 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"knative.dev/pkg/configmap/informer"
+	knativeinjection "knative.dev/pkg/injection"
+	"knative.dev/pkg/injection/sharedmain"
+	"knative.dev/pkg/logging"
+	"knative.dev/pkg/signals"
+	"knative.dev/pkg/system"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	nodev1alpha1 "github.com/aws/aws-node-termination-handler/api/v1alpha1"
 	"github.com/aws/aws-node-termination-handler/controllers"
+	"github.com/go-logr/zapr"
 	//+kubebuilder:scaffold:imports
 )
 
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
+const component = "controller"
+
+var scheme = runtime.NewScheme()
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
@@ -57,48 +63,55 @@ func main() {
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	config := ctrl.GetConfigOrDie()
+	config.UserAgent = "aws-node-termination-handler"
+	ctx, startInformers := knativeinjection.EnableInjectionOrDie(signals.NewContext(), config)
+	rootLogger, atomicLevel := sharedmain.SetupLoggerOrDie(ctx, component)
+	ctx = logging.WithLogger(ctx, rootLogger)
+	ctrlLogger := rootLogger.With("component", component)
+	cmw := informer.NewInformedWatcher(kubernetes.NewForConfigOrDie(config), system.Namespace())
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	ctrl.SetLogger(zapr.NewLogger(rootLogger.Desugar()))
+	rest.SetDefaultWarningHandler(&logging.WarningHandler{Logger: rootLogger})
+	sharedmain.WatchLoggingConfigOrDie(ctx, cmw, rootLogger, atomicLevel, component)
+
+	if err := cmw.Start(ctx.Done()); err != nil {
+		ctrlLogger.With("error", err).Fatal("failed to watch logging configuration")
+	}
+	startInformers()
+
+	mgr, err := ctrl.NewManager(config, ctrl.Options{
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "202c66c6.k8s.aws",
+		LeaderElectionID:       "aws-node-termination-handler.k8s.aws",
+		Logger:                 zapr.NewLogger(logging.FromContext(ctx).Desugar()),
 	})
 	if err != nil {
-		setupLog.Error(err, "unable to start manager")
-		os.Exit(1)
+		ctrlLogger.With("error", err).Fatal("unable to start manager")
 	}
 
 	if err = (&controllers.TerminatorReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Terminator")
-		os.Exit(1)
+		ctrlLogger.With("error", err).Fatalw("unable to create controller")
 	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up health check")
-		os.Exit(1)
+		ctrlLogger.With("error", err).Fatal("unable to set up health check")
 	}
 	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up ready check")
-		os.Exit(1)
+		ctrlLogger.With("error", err).Fatal("unable to set up ready check")
 	}
 
-	setupLog.Info("starting manager")
+	ctrlLogger.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
-		os.Exit(1)
+		ctrlLogger.With("error", err).Fatal("problem running manager")
 	}
 }
