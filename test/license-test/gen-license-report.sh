e3258a8f60f5f58e6d1ddbbcb0a654d83842ecb4
Bryan™ <61433408+brycahta@users.noreply.github.com>
2022-03-18 15:17:56 -0500

    Update go & lib versions in Dockerfiles (#605)
    * align Dockerfile go versions with app
    
    * replace go get with go install

--- test/license-test/gen-license-report.sh
+++ test/license-test/gen-license-report.sh
@@ -7,7 +7,7 @@ mkdir -p $BUILD_DIR
 GOBIN=$(go env GOPATH | sed 's+:+/bin+g')/bin
 export PATH="$PATH:$GOBIN"
 
-go get github.com/mitchellh/golicense
+go install github.com/mitchellh/golicense@v0.2.0
 go build -o $BUILD_DIR/nth $SCRIPTPATH/../../.
 golicense -out-xlsx=$BUILD_DIR/report.xlsx $SCRIPTPATH/license-config.hcl $BUILD_DIR/nth
 
